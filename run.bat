8b136a09b84346fb5c31b30016bd9ae9378b6e33
JaredDC <jared.dc@outlook.com>
2021-05-15 19:38:17 +0800

    名人名言里双引号错了。

--- run.bat
+++ run.bat
@@ -1,14 +1,15 @@
 @echo off
 
-::copy SourceHanSerif-Bold.ttc Release
+copy CelebrityQuotes.txt Release
 
 cd Release
 .\quotes_emoticons_generator.exe
 
-xcopy "%USERPROFILE%\Pictures\quotes_emoticons" ..\quotes_emoticons /s /y /i /q
-xcopy ..\quotes_emoticons quotes_emoticons /s /y /i /q
-
 cd ..
+xcopy "%USERPROFILE%\Pictures\quotes_emoticons" quotes_emoticons /s /y /i /q
+xcopy quotes_emoticons Release\quotes_emoticons /s /y /i /q
+
+del Release\quotes_emoticons_generator.7z
 "C:\Program Files\7-Zip\7z.exe" a quotes_emoticons_generator.7z Release
 move /y quotes_emoticons_generator.7z Release\quotes_emoticons_generator.7z
 
